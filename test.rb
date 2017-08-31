a1802917f2c074928f50dc19565928c2b6b77d19
IlkhamGaysin <ilgamgaysin@gmail.com>
2017-08-31 22:35:35 +0300

    Initial commit

---
+++ test.rb
@@ -0,0 +1,56 @@
+begin
+  require "bundler/inline"
+rescue LoadError => e
+  $stderr.puts "Bundler version 1.10 or later is required. Please update your Bundler"
+  raise e
+end
+
+gemfile(true) do
+  source "https://rubygems.org"
+  gem "rails",'~> 5.0.2', github: "rails/rails"
+  gem "arel", github: "rails/arel"
+  gem "sqlite3"
+end
+
+require "active_record"
+require "minitest/autorun"
+require "logger"
+
+# This connection will do for database-independent bug reports.
+ActiveRecord::Base.establish_connection(adapter: "sqlite3", database: ":memory:")
+ActiveRecord::Base.logger = Logger.new(STDOUT)
+
+ActiveRecord::Schema.define do
+  create_table :posts, force: true do |t|
+  end
+
+  create_table :comments, force: true do |t|
+    t.integer :post_id
+  end
+end
+
+class Post < ActiveRecord::Base
+  has_many :comments
+
+  accepts_nested_attributes_for :comments
+end
+
+class Comment < ActiveRecord::Base
+  belongs_to :post
+end
+
+class BugTest < Minitest::Test
+  def test_association_stuff
+    post = Post.create!
+    comment = Comment.create!
+    post.comments << comment
+
+    assert_equal 1, post.comments.count
+    assert_equal 1, Comment.count
+    assert_equal post.id, Comment.first.post.id
+
+    assert_nothing_raised ActiveRecord::RecordNotFound, "Couldn't find Comment with ID= #{comment.id} for post with ID=#{post.id}" do
+      post.update(comments_attributes: { id: " #{comment.id}" })
+    end
+  end
+end
